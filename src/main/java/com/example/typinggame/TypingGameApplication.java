9199a26b3fed40d953d83b1aa32c44947e76fea6
dharani-tharanm <dharanitharan2019@gmail.com>
2024-11-06 17:21:09 +0530

    Changed the UTF Encoding

--- src/main/java/com/example/typinggame/TypingGameApplication.java
+++ src/main/java/com/example/typinggame/TypingGameApplication.java
@@ -1,4 +1,4 @@
-﻿package com.example.typinggame;
+package com.example.typinggame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
